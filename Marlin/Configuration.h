9b44a5099ca3e206008f76d3253adfa54f1f8aaf
Stephen Mott <stevem@SMUTILS-MAC9-2.local>
2019-02-05 23:04:19 +0000

    Switched Case light defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -828,7 +828,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 90  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 1.350   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER 0.80   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 30
