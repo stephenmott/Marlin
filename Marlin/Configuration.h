a1b4fe48e5878265e7e51bfe70a3816298bc213f
Stephen Mott <s.mott@smutils.com>
2019-09-12 14:27:53 +0100

    Config updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1474,7 +1474,7 @@
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    100
+#define PREHEAT_2_TEMP_BED    110
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_3_LABEL       "PETG"
