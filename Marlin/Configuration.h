210f9b33ed03d997f6d2edbd18a649f712fcdd92
Stephen Mott <s.mott@smutils.com>
2019-12-03 23:08:08 +0000

    Changed Probe Offset to 1.65

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -942,7 +942,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 70, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 70, 1.65 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
